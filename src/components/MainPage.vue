a1a72bd38122771b2d5f1422ea8837b30e96b097
jangseop <kgh010529@naver.com>
2023-02-09 00:41:56 +0900

    add password memory

--- src/components/MainPage.vue
+++ src/components/MainPage.vue
@@ -10,14 +10,26 @@ export default {
             password: "hunloveseop",
             isLogin: false,
             type: 0,
+            memoryPassword: false,
         }
     },
     mounted() {
         this.isLogin = false;
+        console.log(this.getCookie("passwordMemory"));
+
+
+        if (this.getCookie("passwordMemory").length > 0) {
+            this.inputPassword = this.getCookie("passwordMemory");
+            this.memoryPassword = true;
+        }
     },
     methods: {
         enter() {
-            if (this.inputPassword === this.password) this.isLogin = true;
+            if (this.inputPassword === this.password) {
+                this.isLogin = true;
+                if (this.memoryPassword) this.setCookie("passwordMemory", this.inputPassword);
+                else this.deleteCookie("passwordMemory");
+            }
             else {
                 this.inputPassword = "";
                 alert("비밀번호 모르네? 나가 그냥");
@@ -26,6 +38,28 @@ export default {
 
         changeType(type) {
             this.type = type;
+        },
+
+        setCookie(cookieName, value) {
+            document.cookie = cookieName + "=" + value;
+        },
+
+        getCookie(cookieName) {
+            cookieName = cookieName + "=";
+            var cookieData = document.cookie;
+            var start = cookieData.indexOf(cookieName);
+            var value = "";
+
+            if (start !== -1) {
+                start += cookieName.length;
+                value = cookieData.substring(start, cookieData.length);
+            }
+
+            return value;
+        },
+
+        deleteCookie(cookieName) {
+            document.cookie = cookieName + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
         }
     }
 }
@@ -34,8 +68,10 @@ export default {
 <template >
     <div v-if="!isLogin">
         <h1>비밀번호 모르면 접속할 생각 꿈도 꾸지마<br>무료로 이용 할 생각 추어도 하지마</h1>
-        <input type="text" v-model="inputPassword">
-        <button @click="enter">접속</button>
+        <input type="password" v-model="inputPassword">
+        <button style="margin-left: 7px;" @click="enter">접속</button>
+        <input style="margin-left: 15px;" type="checkbox" id="memory-password" v-model="memoryPassword">
+        <label style="margin-left: 1px;" for="memory-password">비밀번호 기억하기</label>
     </div>
     <div v-else>
         <h1>엔조이런너 운영자 귀여운 훈이의 텍스트 편집기</h1>
